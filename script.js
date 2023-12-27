8350d6efdaa3b81d3f05b7e271216093befb3cfe
Taiwo Omoya <taiwoomoya272@gmail.com>
2023-12-27 08:41:45 -0500

    feat: Function 2
    I added funtion 2. The name of the funtion is playRound. And its Function
    is to determine the winner of the game after one round. I will create
    another function that determines the winner after 5 iterations of
    playRound.

--- script.js
+++ script.js
@@ -7,10 +7,36 @@ function getComputerChoice() {
 
   switch (randomNumber) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
+  }
+}
+
+let computerChoice = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  let computerSelection = computerChoice;
+  playerSelection = playerSelection.toLowerCase();
+  //For ties
+  if (playerSelection === computerSelection) {
+    return "It's a tie! Play again";
+  }
+
+  //Determine the winner
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper";
+  } else if (PlayerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors";
   }
 }
